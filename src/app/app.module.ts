850cf9984ce2b2b306078468e40441c1079b94d0
Ahmed Hassan <Ahmed.Hassan@Qcentriseg.com>
2020-05-28 02:55:24 +0200

    adding basic design till skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { BriefIntroComponent } from './components/brief-intro/brief-intro.component';
+import { WhatIDoComponent } from './components/what-i-do/what-i-do.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { SquareCardComponent } from './components/square-card/square-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    BriefIntroComponent,
+    WhatIDoComponent,
+    SkillsComponent,
+    SquareCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
