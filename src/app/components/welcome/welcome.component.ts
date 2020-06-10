bf1af4010a64fc87500b4f4a93f47ba4d5871041
Ahmed Hassan <Ahmed.Hassan@Qcentriseg.com>
2020-06-10 23:14:01 +0200

    adding video

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
 })
-export class WelcomeComponent implements OnInit {
-
-  constructor() { }
+export class WelcomeComponent implements OnInit, OnDestroy {
+  constructor() {}
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.scrollEvent, true);
   }
-
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scrollEvent, true);
+  }
+  scrollEvent = (event: any): void => {
+    let scrollValue = event.target.scrollingElement.scrollTop;
+  }
+  transformFn(){
+    return 'translate3d(10px, 0, 100px);';
+  }
 }
