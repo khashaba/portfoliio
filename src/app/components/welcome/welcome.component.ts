c55054ba2a7d825bf728183611d422360889d6ed
Ahmed Hassan <Ahmed.Hassan@Qcentriseg.com>
2020-06-12 01:09:57 +0200

    update scroll

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -10,7 +10,7 @@ export class WelcomeComponent implements OnInit, OnDestroy {
   //translateIValue: number;
   // translateConstructValue: number;
   // translateThingsValue: number;
-  hideSecondHeader = {hideHeader: true , secondHeader: true};
+  hideSecondHeader = { hideHeader: true };
   constructor() {}
 
   ngOnInit(): void {
@@ -20,19 +20,22 @@ export class WelcomeComponent implements OnInit, OnDestroy {
     window.removeEventListener('scroll', this.scrollEvent, true);
   }
   scrollEvent = (event: any): void => {
-    this.translateHeyValue = event.target.scrollingElement.scrollTop * -1.5;
-    this.translateNameValue = event.target.scrollingElement.scrollTop * 2.5;
-    //this.translateIValue = event.target.scrollingElement.scrollTop;
-    if ( event.target.scrollingElement.scrollTop >= 116 ){
+    let scrollValue = event.target.scrollingElement.scrollTop;
+    if (scrollValue < 1300) {
+      console.log(scrollValue);
+      this.translateHeyValue = event.target.scrollingElement.scrollTop * -1.5;
+      this.translateNameValue = event.target.scrollingElement.scrollTop * 2.5;
+      //this.translateIValue = event.target.scrollingElement.scrollTop;
 
-     // this.translateConstructValue = event.target.scrollingElement.scrollTop * -1.5;
-     // this.translateThingsValue = event.target.scrollingElement.scrollTop * -1.5;
-    this.hideSecondHeader.hideHeader = false;
-    }else{
-     this.hideSecondHeader.hideHeader = true;
-
+      if (event.target.scrollingElement.scrollTop >= 116) {
+        // this.translateConstructValue = event.target.scrollingElement.scrollTop * -1.5;
+        // this.translateThingsValue = event.target.scrollingElement.scrollTop * -1.5;
+        this.hideSecondHeader.hideHeader = false;
+      } else {
+        this.hideSecondHeader.hideHeader = true;
+      }
     }
-  }
+  };
   transformFn() {
     return 'translate3d(10px, 0, 100px);';
   }
